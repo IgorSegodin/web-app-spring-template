c547253c0644a147609a6aaf212ebc2f7a756d14
Igor Segodin <i.segodin@profitsoft.ua>
2017-09-27 14:40:15 +0300

    Fix query-dsl

--- readme.txt
+++ readme.txt
@@ -9,6 +9,16 @@ Main technologies:
     Query DSL
     Thymeleaf
 
+For Intellij Idea
+
+1) Go to Preferences -> Build, Execution, Deployment -> Annotation Processors
+	Check Enable annotation processing checkbox
+	In "Store generated sources relative to:" select Module content root
+2) Add folder "src/main/generated" as sources
+
 TODO:
     Webpack 2
     E-tag resource filter
+
+com/moowork/gradle/gradle-node-plugin/1.1.0/
+classpath("com.moowork.gradle:gradle-node-plugin:1.1.0")
--- src/main/java/org/app/template/data/service/impl/UserServiceImpl.java
+++ src/main/java/org/app/template/data/service/impl/UserServiceImpl.java
@@ -1,7 +1,8 @@
 package org.app.template.data.service.impl;
 
-import com.querydsl.core.types.dsl.PathBuilder;
-import com.querydsl.core.types.dsl.StringPath;
+import javax.servlet.http.HttpServletRequest;
+
+import org.app.template.data.domain.QUser;
 import org.app.template.data.domain.User;
 import org.app.template.data.repository.UserRepository;
 import org.app.template.data.service.UserService;
@@ -14,44 +15,38 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Component
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	@Autowired
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        User user = userRepository.findByEmail(email);
-        // Example of Query API. TODO create builder and possible use generated classes (like QUser) to use compile check for entity fields
-        PathBuilder<User> entityPath = new PathBuilder<>(User.class, "user");
-        StringPath emailPath = entityPath.getString("email");
-        User user = userRepository.findOne(emailPath.eq(email));
-        if (user == null) {
-            throw new UsernameNotFoundException("Can't find user with email: " + email);
-        }
-        return user;
-    }
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findOne(QUser.user.email.eq(email));
+		if (user == null) {
+			throw new UsernameNotFoundException("Can't find user with email: " + email);
+		}
+		return user;
+	}
 
-    @Override
-    public User getCurrentUser() throws AuthenticationCredentialsNotFoundException {
-        if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null) {
-            return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        } else {
-            throw new AuthenticationCredentialsNotFoundException("Not authenticated.");
-        }
-    }
+	@Override
+	public User getCurrentUser() throws AuthenticationCredentialsNotFoundException {
+		if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null) {
+			return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		} else {
+			throw new AuthenticationCredentialsNotFoundException("Not authenticated.");
+		}
+	}
 
-    protected String getCurrentIp() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
-                .getRequest();
-        return request.getRemoteAddr();
-    }
+	protected String getCurrentIp() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+				.getRequest();
+		return request.getRemoteAddr();
+	}
 }
